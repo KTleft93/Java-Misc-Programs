53a4aa0967fa838e439209e27b7aaa36c650bcf3
KTleft93 <52896162+KTleft93@users.noreply.github.com>
2020-06-29 00:51:54 -0400

    Add files via upload

---
+++ CountKeywords.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class CountKeywords {
+public static void main(String[] args) {
+
+String text =  "It took a long time to code this, it took a short time to type this ";
+
+MyHashedMap<String, Integer> map = new MyHashedMap<>();
+
+
+String[] words = text.split("[ \n\t\r.,;:!?(){}]");
+for (int i = 0; i < words.length; i++) {
+String key = words[i].toLowerCase();
+
+if (key.length() > 0) {
+if (!map.containsKey(key)) {
+map.put(key, 1);
+}
+else {
+int value = map.get(key);
+value++;
+map.put(key, value);
+}
+}
+}
+
+Set<MyHashedMap.Entry<String, Integer>> entrySet = map.entrySet();
+
+
+for (MyMap.Entry<String, Integer> entry: entrySet)
+System.out.println(entry.getKey() + "\t" + entry.getValue());
+}
+}
