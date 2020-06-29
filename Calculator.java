4261824dab791833fded1dbe28ae8f7cca7a2ed3
KTleft93 <52896162+KTleft93@users.noreply.github.com>
2020-06-29 00:49:51 -0400

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class Calculator {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Enter 1st number:");
+		Scanner red=new Scanner(System.in);
+		int num1=red.nextInt();
+		System.out.println("Enter 2nd number:");
+		int num2=red.nextInt();
+		String op="";
+		System.out.println("Select your operation: ");
+		op=red.next();
+		if (op.equals("+")) {
+		System.out.print("Answer:"+ (num1+num2));
+		}
+		if (op.equals("-")) {
+			System.out.print("Answer:"+ (num1-num2));
+			}
+		if (op.equals("/")) {
+			System.out.print("Answer:"+ (num1/num2));
+			}
+		if (op.equals("*")) {
+			System.out.print("Answer:"+ (num1*num2));
+			}
+		else {
+			System.out.println("ERROR");
+		}
+		
+	}
+
+}
